916545b644029e3549e354e14d15b59cd720579e
anujthakor09 <thakoranuj09@gmail.com>
2026-07-07 09:35:46 +0530

    Implement counting of even and odd numbers in array
    Initialize even and odd counters, read array elements, and count even and odd numbers.

---
+++ DSA/Arrays/005_Count_even-odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+  int even;
+  int odd;
+  int arr[10];
+  cout<<"enter the elements of arr"<<endl;
+  for(int i=0;i<10;i++){
+    cin>>arr[i];
+  }
+  for(int i=1;i<10;i++){
+    if(arr[i]%2==0) even++;
+    else odd++;
+  }
+  cout<<"even = "<<even<<endl;
+  cout<<"odd = "<<odd<<endl;
+  return 0;
+}
